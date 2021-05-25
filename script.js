2eda42b27b031651b80568d67e482dccfdb49d59
ThomasP850 <24Tpeterson@gmail.com>
2021-05-24 23:02:02 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const contentHeight = 1244;
+let popUpShown = false;
+const popUp = document.getElementById("subscribe-popup");
+const otherContent = document.getElementById("everything-else");
+let scrollY = window.scrollY;
+// Called periodically to check the page position and adapt the pop up if necesarry.
+setInterval(check, 50);
+function check() {
+    console.log("Scroll: " + scrollY);
+    scrollY = window.scrollY;
+    if(popUpShown && scrollY < contentHeight){
+        togglePopup();
+    } else if(!popUpShown && scrollY >= contentHeight) {
+        togglePopup();
+    }
+}
+function togglePopup() {
+    if(popUpShown) {
+        popUpShown = false;
+        popUp.style.display = 'none';
+        otherContent.style.filter = 'none'
+    } else {
+        popUpShown = true;
+        popUp.style.display = 'flex';
+        otherContent.style.filter = 'blur(7px)'
+    }
+}
